5b06223c72bd66c293e9daf62675df304cf9e01e
des6747 <des6747@gmail.com>
2024-12-19 11:29:30 +0530

    Second Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   root "main#index"
   get "main/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
